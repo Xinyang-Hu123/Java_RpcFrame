544fcc8456247cb2dbb03b19f429d7b1be88d1ea
huxinyang <547397778@qq.com>
2026-04-14 14:29:05 +0800

    集成 Nacos 实现服务注册与发现，引入本地服务缓存并通过 subscribe 监听服务变化动态更新，实现随机、轮询两种负载均衡策略

--- src/main/java/Nacos/NacosServiceRegistry.java
+++ src/main/java/Nacos/NacosServiceRegistry.java
@@ -1,22 +1,27 @@
 package Nacos;
 
+import Client.Cache.ServiceCache;
+import Client.loadBalance.RandomLoadBalance;
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import com.alibaba.nacos.api.naming.pojo.Instance;
 
+import java.util.List;
 import java.util.Properties;
 
 public class NacosServiceRegistry {
 
     private final NamingService namingService;
-
+    private ServiceCache serviceCache;
     public NacosServiceRegistry() throws NacosException {
         Properties properties = new Properties();
         properties.setProperty("serverAddr", "127.0.0.1:8848");
         properties.put("username", "nacos");
         properties.put("password", "tlThF01zY1BO");
         this.namingService = NacosFactory.createNamingService(properties);
+        this.serviceCache = new ServiceCache(new RandomLoadBalance());
     }
 
     //注册服务
@@ -26,8 +31,31 @@ public class NacosServiceRegistry {
 
     //发现服务
     public Instance discovery(String serviceName) throws NacosException {
-        return namingService.selectOneHealthyInstance(serviceName);
+        //判断当前实例是否存在
+        Instance instance = serviceCache.getCache(serviceName);
+        if (instance != null) {
+            return instance;
+        }
+
+        //从nacos获取实例
+        List<Instance> instances = namingService.getAllInstances(serviceName);
+        if (instances == null || instances.isEmpty()) {
+            return null;
+        }
+
+        //存进缓存
+        serviceCache.addCache(serviceName, instances);
+
+        namingService.subscribe(serviceName, event -> {
+            NamingEvent namingEvent = (NamingEvent) event;
+            serviceCache.addCache(serviceName, namingEvent.getInstances());
+        });
+
+        return instances.get(0);
     }
+
+
+
 
 
 
